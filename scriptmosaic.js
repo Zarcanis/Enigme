e585c1207a25feb47fe6e37658c90e21076f8cba
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-23 18:56:40 +0100

    Create scriptmosaic.js

---
+++ scriptmosaic.js
@@ -0,0 +1,21 @@
+// Sélectionnez tous les éléments de l'image avec la classe "puzzle-piece"
+const puzzlePieces = document.querySelectorAll('.puzzle-piece');
+
+// Bouclez à travers chaque élément d'image et ajoutez un gestionnaire d'événements de clic
+puzzlePieces.forEach(piece => {
+  piece.addEventListener('click', () => {
+    // Vérifiez si l'image cliquée a la bonne réponse
+    if (piece.alt === 'image7') {
+      // Affichez un message de félicitations et redirigez vers la page suivante après 2 secondes
+      const message = document.getElementById('message');
+      message.textContent = 'Bravo, c\'est la bonne réponse !';
+      setTimeout(() => {
+        window.location.href = 'lettre.html';
+      }, 2000);
+    } else {
+      // Affichez un message d'erreur si la réponse est incorrecte
+      const message = document.getElementById('message');
+      message.textContent = 'Désolé, ce n\'est pas la bonne réponse. Essayez encore !';
+    }
+  });
+})
