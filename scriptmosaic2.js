9a133d734bcc8c044f1824bce8ad57b6d07871a3
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-23 18:58:51 +0100

    Create scriptmosaic2.js

---
+++ scriptmosaic2.js
@@ -0,0 +1,15 @@
+var options = document.querySelectorAll(".enigma-option");
+var message = document.getElementById("message");
+
+for (var i = 0; i < options.length; i++) {
+	options[i].addEventListener("click", function() {
+		if (this.alt === "image true") {
+			message.innerHTML = "See ? Being different is the key";
+			setTimeout(function() {
+				window.location.href = "flambeau.html";
+			}, 2000);
+		} else {
+			message.innerHTML = "What are you doing, clicking at random?!";
+		}
+	});
+}
