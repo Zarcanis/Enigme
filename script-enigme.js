428bff35b9746b47d423659b299b7dfcd66b8876
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-23 18:54:30 +0100

    Create script-enigme.js

---
+++ script-enigme.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const passwordInput = document.querySelector('#password');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const password = passwordInput.value.toLowerCase();
+  
+  if (password === 'secret') {
+    window.location.href = 'mosaic.html';
+  } else {
+    alert('Aller Audrey, fais un effort !');
+  }
+});
